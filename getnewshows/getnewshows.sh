bd264b16ea934ef545395a87f9127f012b641fc8
frownbreaker <24481030+frownbreaker@users.noreply.github.com>
2018-02-11 21:59:49 +0000

    Initial scripts
    Housekeeping bash and python scripts

---
+++ getnewshows/getnewshows.sh
@@ -0,0 +1,3 @@
+/root/feedtransmission.py --transmission-host 192.168.0.xxx --transmission-port 9091 http://showrss.info/user/xxxxx.rss?magnets=true&namespaces=true&name=null&quality=null&re=null
+
+
---
+++ purgetvroom/purge-tvroom.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys, os
+import feedparser
+import transmissionrpc
+import argparse
+import datetime
+
+# Read the torrents on the server...
+kc=0
+tc=transmissionrpc.Client('192.168.0.xxx',port=9091)
+t=tc.get_torrents()
+for key in t:
+#for torrent in tc.items():
+	if key.isFinished:
+		print 'Removing Seed Complete', key.name
+		tc.remove_torrent(key.id)
+	else:
+		if key.percentDone == 1:
+			print 'Removing Download Complete',key.name
+			kc=kc+1
+			tc.remove_torrent(key.id) #Retains data. Torrent is removed
+
+
+
+if kc>0:
+	sttime = datetime.datetime.now().strftime('%Y%m%d_%H:%M:%S - ')
+	le = "Purge run. " + str(kc) +" file(s) removed."
+	log = '/root/purgetvroom.log'
+	with open(log, 'a') as logfile:
+		logfile.write(sttime + le + '\n')
+
